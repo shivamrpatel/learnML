4a6af9a62005117bc7275b0a0c648fb8e3f03fc6
Shivam Patel <shivam.p36181@gmail.com>
2022-07-07 19:15:20 -0400

    Initial Commit

---
+++ learnML/learnMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnMLApp.swift
+//  learnML
+//
+//  Created by Shivam on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct learnMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
